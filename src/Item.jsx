3412bb823321c7991c7daadd716a5051af0048ad
AviadDerli <aviad@workin.co.il>
2024-03-31 18:17:08 +0300

    css

--- src/Item.jsx
+++ src/Item.jsx
@@ -4,7 +4,7 @@ export default function Item({item,total,setTotal}) {
     let {barcode,name,emoji,price} =item;
 
   return (
-    <div>
+    <div className='card'>
         <h2>{name}</h2>
         <h1>{emoji}</h1>
         <h2>{price}</h2>
