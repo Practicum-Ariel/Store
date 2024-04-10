f93c247645d64205470893f63b0f012f847fe92d
AviadDerli <aviad@workin.co.il>
2024-04-10 15:35:39 +0300

    popup and custom hooks

---
+++ src/Popup.jsx
@@ -0,0 +1,12 @@
+import useMyContext from './context/DataContext'
+
+export default function Popup() {
+    const { popup, setPopup } = useMyContext()
+
+    return <div
+        className='overlay'
+        onClick={() => setPopup()}>
+        <button onClick={() => setPopup()}>X</button>
+        <div className='popup' onClick={(e) => { e.stopPropagation() }}>{popup}</div>
+    </div>
+}
